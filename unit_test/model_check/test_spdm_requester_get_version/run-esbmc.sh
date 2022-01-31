e1f74e62ae78bb2d605382b36180b428d13e8f9f
Fedor Shmarov <shmarovfedor@mail.ru>
2022-01-31 15:24:37 +0000

    [model_check] added missing libraries into the ESBMC script

--- unit_test/model_check/test_spdm_requester_get_version/run-esbmc.sh
+++ unit_test/model_check/test_spdm_requester_get_version/run-esbmc.sh
@@ -2,24 +2,28 @@ LIBSPDM="$(cd "$( dirname "${BASH_SOURCE[0]}" )"/../../.. &> /dev/null && pwd)"
 
 $HOME/esbmc/release/bin/esbmc \
  --force-malloc-success \
- --no-library \
  --goto-unwind \
- --k-step 40000 --unlimited-k-steps \
+ --k-step 10 --unlimited-k-steps \
  --k-induction --max-inductive-step 3 \
- --z3 \
  $LIBSPDM/unit_test/model_check/test_spdm_requester_get_version/get_version.c \
  $LIBSPDM/unit_test/spdm_unit_test_common/common.c \
+ $LIBSPDM/unit_test/spdm_transport_test_lib/common.c \
+ $LIBSPDM/unit_test/spdm_transport_test_lib/test.c \
  $LIBSPDM/library/spdm_requester_lib/libspdm_req_get_version.c \
  $LIBSPDM/library/spdm_common_lib/libspdm_com_context_data.c \
  $LIBSPDM/library/spdm_common_lib/libspdm_com_context_data_session.c \
  $LIBSPDM/library/spdm_secured_message_lib/libspdm_secmes_context_data.c \
+ $LIBSPDM/library/spdm_secured_message_lib/libspdm_secmes_encode_decode.c \
  $LIBSPDM/library/spdm_crypt_lib/libspdm_crypt_crypt.c \
  $LIBSPDM/library/spdm_requester_lib/libspdm_req_send_receive.c \
  $LIBSPDM/library/spdm_requester_lib/libspdm_req_handle_error_response.c \
  $LIBSPDM/library/spdm_common_lib/libspdm_com_support.c \
  $LIBSPDM/os_stub/memlib/copy_mem.c \
  $LIBSPDM/os_stub/memlib/zero_mem.c \
- $LIBSPDM/os_stub/debuglib/debuglib.c \
+ $LIBSPDM/os_stub/memlib/compare_mem.c \
+ $LIBSPDM/os_stub/debuglib_null/debuglib.c \
+ $LIBSPDM/os_stub/cryptlib_null/rand/rand.c \
+ $LIBSPDM/os_stub/cryptlib_null/hash/sha.c \
  -I$LIBSPDM/unit_test/spdm_unit_test_common/ \
  -I$LIBSPDM/unit_test/include/ \
  -I$LIBSPDM/include/ \
@@ -28,7 +32,7 @@ $HOME/esbmc/release/bin/esbmc \
  -I$LIBSPDM/include/internal/ \
  -I$LIBSPDM/include/library/ \
  -I$LIBSPDM/os_stub/spdm_device_secret_lib_sample/ \
- -I$LIBSPDM/os_stub/cryptlib_mbedtls/ \
+ -I$LIBSPDM/os_stub/cryptlib_null/ \
  -I$LIBSPDM/os_stub/include/ \
  -I$LIBSPDM/os_stub/mbedtlslib/include \
  -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/ \
